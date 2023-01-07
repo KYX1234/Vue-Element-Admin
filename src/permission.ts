b8409ee6522dc9af81c0af6088b23a2912725f13
kyx <1059784729@qq.com>
2023-01-07 18:32:19 +0800

    fix: 修改权限判断

--- src/permission.ts
+++ src/permission.ts
@@ -1,53 +1,52 @@
-import router from '@/router';
-import { ElMessage } from 'element-plus';
-import {useUserStore,useRouteStore} from '@/store';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import { clearPending } from "@/utils/request"
+import router from '@/router'
+import { ElMessage } from 'element-plus'
+import { useUserStore, useRouteStore } from '@/store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { clearPending } from '@/utils/request'
 
-NProgress.configure({ showSpinner: false }); // 进度环显示/隐藏
+NProgress.configure({ showSpinner: false }) // 进度环显示/隐藏
 
 // 白名单路由
-const whiteList = ['/login'];
+const whiteList = ['/login']
 
 router.beforeEach(async (to, from, next) => {
+
   clearPending()
-  NProgress.start();
-  const hasToken = useUserStore().token;
-  if (hasToken) {
-    // 登录成功，跳转到首页
-    if (to.path === '/login') {
-      next({ path: '/' });
-      NProgress.done();
-    } else {
-      const hasRoutes = useRouteStore().addRoutes.length > 0;
-      if (hasRoutes) {
-          next();
-      } else {
-        try {
-          await useRouteStore().initRoutes();
-          next({ ...to, replace: true });
+  
+	const hasToken = useUserStore().token
 
-        } catch (error) {
-          // 移除 token 并跳转登录页
-          await useUserStore().resetToken();
-          ElMessage.error((error as any) || 'Has Error');
-          next(`/login?redirect=${to.path}`);
-          NProgress.done();
-        }
-      }
-    }
-  } else {
-    // 未登录可以访问白名单页面(登录页面)
-    if (whiteList.indexOf(to.path) !== -1) {
-      next();
-    } else {
-      next(`/login?redirect=${to.path}`);
-      NProgress.done();
-    }
-  }
-});
+	NProgress.start()
 
+	if (to.path === '/login') {
+		if (!hasToken) return next()
+		else return next(from.fullPath)
+	}
+
+	// 判断是否有 Token，没有重定向到 login
+	if (!hasToken) return next(`/login?redirect=${to.path}`);
+
+	// 如果没有菜单列表，重载菜单列表并添加动态路由
+	const hasRoutes = useRouteStore().addRoutes.length > 0;
+	if (!hasRoutes) {
+    await useRouteStore().initRoutes();
+		return next({ ...to, replace: true })
+	}
+
+	next()
+})
+
+/**
+ * @description 路由跳转结束
+ * */
 router.afterEach(() => {
-  NProgress.done();
-});
+	NProgress.done()
+})
+
+/**
+ * @description 路由跳转错误
+ * */
+router.onError(error => {
+	NProgress.done()
+	console.warn('路由错误', error.message)
+})
