1b39a1d89c3cb04e58e3ad1d444d84bc27c1cffc
kyx <1059784729@qq.com>
2023-03-22 15:52:57 +0800

    chore: 更新

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import directives from "@/directives/index";
-import 'default-passive-events'
 import 'element-plus/dist/index.css'
 import '@/styles/index.scss'
 import '@/permission'
